bb88c353c058f537061ba4f6567fae2882c43f11
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-01 19:45:15 -0600

    set up initial testing environment

---
+++ tests/mainTest.js
@@ -0,0 +1,8 @@
+//main test file for travis integration
+const {expect} = require("chai");
+
+describe('Main test', () => {
+	it("Should return true for true", ()=>{
+		expect(false).to.equal(false);
+	});
+});
