e03794cede035da6d2781fdbd0d24a0c593dafb1
Ben Bledsoe <bledsoedesigns@gmail.com>
2016-02-01 19:56:25 -0600

    added simple tests for test function

--- tests/mainTest.js
+++ tests/mainTest.js
@@ -1,8 +1,16 @@
 //main test file for travis integration
 const {expect} = require("chai");
+const mainJs = require("../src/main.js");
 
-describe('Main test', () => {
+describe('Base test', () => {
 	it("Should return true for true", ()=>{
 		expect(false).to.equal(false);
+	});
+});
+
+describe('Main js test', () => {
+	it("Should return true for dayrin", ()=>{
+		expect(mainJs("bill")).to.equal(false);
+		expect(mainJs("dayrin")).to.equal(true);
 	});
 });
